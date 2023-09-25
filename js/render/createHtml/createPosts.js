59f4f81cb07c5f2e610c36408c0fc4d50b9d94ac
Tokle89 <fredriktokle@gmail.com>
2023-09-25 17:57:34 +0200

    fixed naming conventions

--- js/render/createHtml/createPosts.js
+++ js/render/createHtml/createPosts.js
@@ -3,12 +3,11 @@ import { createElement } from "./createHtmlFunction.js";
 const postContainer = document.getElementById("posts-container");
 console.log(postContainer);
 
-export const createPosts = (posts) => {
-  const { author } = posts;
+export const createPosts = ({ author, title, body, tags, media, created }) => {
   const post = createElement("div", ["post", "p-3", "mb-3", "bg-white"]);
-  const authorContainer = createPostAuthor(author);
-
-  post.append(authorContainer);
+  const postAuthor = createPostAuthor(author);
+  const postContent = createPostContent(title, body, tags, media, created);
+  post.append(postAuthor, postContent);
   postContainer.append(post);
 };
 
@@ -18,10 +17,29 @@ const createPostAuthor = ({ name, avatar }) => {
   if (avatar) {
     const img = createElement("img", undefined, undefined, undefined, undefined, avatar, name);
     element.append(img);
+  } else {
+    const img = createElement("img", undefined, undefined, undefined, undefined, "../images/profile.jpg", name);
+    element.append(img);
   }
 
   const h2 = createElement("h2", ["text-primary", "fs-6", "fw-bold", "ps-2", "pt-2"], undefined, name);
 
   element.append(h2);
+  return element;
+};
+
+const createPostContent = (title, body, tags, media, created) => {
+  const element = createElement("div");
+  if (media) {
+    const img = createElement("img", ["p-2"], undefined, undefined, undefined, media);
+    element.append(img);
+  }
+
+  const h2 = createElement("h3", ["pt-2", "fs-6", "fw-bold"], undefined, title);
+  const p = createElement("p", ["text-muted"], undefined, body);
+  const commentBtn = createElement("button", ["btn", "btn-info"], undefined, "Comments");
+  const commentsContainer = createElement("div", undefined, [commentBtn]);
+
+  element.append(h2, p, commentsContainer);
   return element;
 };
